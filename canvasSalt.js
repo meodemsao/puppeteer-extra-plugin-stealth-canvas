60c24fd8b0c4a66a37d918993b26e33b216452d8
bangvc <bangvc@vgroup.vn>
2023-12-17 16:52:59 +0700

    fix ref

---
+++ canvasSalt.js
@@ -0,0 +1,17 @@
+function buildCanvasSalt() {
+  const random = (list) => {
+    const min = 0;
+    const max = list.length;
+    return list[Math.floor(Math.random() * (max - min)) + min];
+  };
+
+  const canvasSalt = [];
+  for (let n = 0; n < 100; ++n) {
+    const r = random([0, 1, 2, 3, 4, 5, 6].map((a) => a - 3));
+
+    canvasSalt.push(r);
+  }
+  return canvasSalt;
+}
+
+module.exports = buildCanvasSalt;
